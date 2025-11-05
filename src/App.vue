6688f7f715f45bcbc237f679f921ccd09acbc53e
wlsdudwjd <jjyabc11@naver.com>
2025-11-05 23:10:00 +0900

    update example4

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <E05ParentComponent />
+  <e06-parent-component/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import E05ParentComponent from "@/components/example3/ParentComponent.vue";
+import E06ParentComponent from "@/components/example4/ParentComponent.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
-    E05ParentComponent,
+    E06ParentComponent
+
   }
 });
 </script>
