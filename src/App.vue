7153e24b3e329c268e83c5df1a63db5ea5706941
wlsdudwjd <jjyabc11@naver.com>
2025-11-05 22:35:04 +0900

    update example1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <E01Instance />
+  <e03-binding />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import E02Reactive from "@/components/example1/E-02-reactive.vue";
 import E01Instance from "@/components/example1/E-01-instance.vue";
+import E03Binding from "@/components/example1/E-03-binding.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
-    E01Instance
+    E03Binding,
   }
 });
 </script>
